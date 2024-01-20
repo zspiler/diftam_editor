0907148ee3d40056f254bb9fa4a092830a62c9d0
Zan Spiler <zan.spiler@celtra.com>
2024-01-20 13:05:10 +0100

    tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+// import 'package:graphview';
+
+import 'package:graphview/graphview.dart';
+// import 'package:graphview/GraphView.dart';
+// import 'package:graphview/edgerenderer/ArrowEdgeRenderer.dart';
+// import 'package:graphview/edgerenderer/EdgeRenderer.dart';
+// import 'package:graphview/forcedirected/FruchtermanReingoldAlgorithm.dart';
+// import 'package:graphview/layered/SugiyamaAlgorithm.dart';
+// import 'package:graphview/layered/SugiyamaConfiguration.dart';
+// import 'package:graphview/layered/SugiyamaEdgeData.dart';
+// import 'package:graphview/layered/SugiyamaEdgeRenderer.dart';
+// import 'package:graphview/layered/SugiyamaNodeData.dart';
+// import 'package:graphview/tree/BuchheimWalkerAlgorithm.dart';
+// import 'package:graphview/tree/BuchheimWalkerConfiguration.dart';
+// import 'package:graphview/tree/BuchheimWalkerNodeData.dart';
+// import 'package:graphview/tree/TreeEdgeRenderer.dart';
 
 final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
@@ -15,45 +31,165 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: CustomPaint(
-          painter: RectangularFlowerPainter(),
-        ),
-      ),
+      // home: Scaffold(
+      //   body: CustomPaint(
+      //     painter: RectangularFlowerPainter(),
+      //   ),
+      // ),
+      home: TreeViewPage(),
     );
   }
 }
 
-class RectangularFlowerPainter extends CustomPainter {
-  Paint green_paint = Paint()..color = Color(0xff00ff00);
-  Paint yellow_paint = Paint()..color = Color(0xffebe834);
-  Paint white_paint = Paint()..color = Color(0xffFFFFFF);
-
+class TreeViewPage extends StatefulWidget {
   @override
-  void paint(Canvas canvas, Size size) {
-    // Draw background
-    // canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), white_paint);
+  _TreeViewPageState createState() => _TreeViewPageState();
+}
 
-    canvas.translate(0, 300); // starting pint
-
-    final paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 4.0
-      ..color = Colors.lime;
-
-    final radius = Radius.circular(20);
-
-    canvas.drawRRect(
-        RRect.fromRectAndRadius(Rect.fromLTWH(20, 40, 100, 100), radius),
-        paint);
-
-    canvas.drawLine(Offset(120, 90), Offset(200, 90), paint);
-
-    canvas.drawRRect(
-        RRect.fromRectAndRadius(Rect.fromLTWH(200, 40, 100, 100), radius),
-        paint);
+class _TreeViewPageState extends State<TreeViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Wrap(
+          children: [
+            Container(
+              width: 100,
+              child: TextFormField(
+                initialValue: builder.siblingSeparation.toString(),
+                decoration: InputDecoration(labelText: "Sibling Separation"),
+                onChanged: (text) {
+                  builder.siblingSeparation = int.tryParse(text) ?? 100;
+                  this.setState(() {});
+                },
+              ),
+            ),
+            Container(
+              width: 100,
+              child: TextFormField(
+                initialValue: builder.levelSeparation.toString(),
+                decoration: InputDecoration(labelText: "Level Separation"),
+                onChanged: (text) {
+                  builder.levelSeparation = int.tryParse(text) ?? 100;
+                  this.setState(() {});
+                },
+              ),
+            ),
+            Container(
+              width: 100,
+              child: TextFormField(
+                initialValue: builder.subtreeSeparation.toString(),
+                decoration: InputDecoration(labelText: "Subtree separation"),
+                onChanged: (text) {
+                  builder.subtreeSeparation = int.tryParse(text) ?? 100;
+                  this.setState(() {});
+                },
+              ),
+            ),
+            Container(
+              width: 100,
+              child: TextFormField(
+                initialValue: builder.orientation.toString(),
+                decoration: InputDecoration(labelText: "Orientation"),
+                onChanged: (text) {
+                  builder.orientation = int.tryParse(text) ?? 100;
+                  this.setState(() {});
+                },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final node12 = Node.Id(r.nextInt(100));
+                var edge =
+                    graph.getNodeAtPosition(r.nextInt(graph.nodeCount()));
+                print(edge);
+                graph.addEdge(edge, node12);
+                setState(() {});
+              },
+              child: Text("Add"),
+            )
+          ],
+        ),
+        Expanded(
+          child: InteractiveViewer(
+              constrained: false,
+              boundaryMargin: EdgeInsets.all(100),
+              minScale: 0.01,
+              maxScale: 5.6,
+              child: GraphView(
+                graph: graph,
+                algorithm:
+                    BuchheimWalkerAlgorithm(builder, TreeEdgeRenderer(builder)),
+                paint: Paint()
+                  ..color = Colors.green
+                  ..strokeWidth = 1
+                  ..style = PaintingStyle.stroke,
+                builder: (Node node) {
+                  // I can decide what widget should be shown here based on the id
+                  var a = node.key!.value as int; // TODO was node.key.value
+                  return rectangleWidget(a);
+                },
+              )),
+        ),
+      ],
+    ));
   }
 
+  Random r = Random();
+
+  Widget rectangleWidget(int a) {
+    return InkWell(
+      onTap: () {
+        print('clicked');
+      },
+      child: Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            boxShadow: [
+              BoxShadow(color: Colors.blue, spreadRadius: 1),
+            ],
+          ),
+          child: Text('Node ${a}')),
+    );
+  }
+
+  final Graph graph = Graph()..isTree = true;
+  BuchheimWalkerConfiguration builder = BuchheimWalkerConfiguration();
+
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
+  void initState() {
+    final node1 = Node.Id(1);
+    final node2 = Node.Id(2);
+    final node3 = Node.Id(3);
+    final node4 = Node.Id(4);
+    final node5 = Node.Id(5);
+    final node6 = Node.Id(6);
+    final node8 = Node.Id(7);
+    final node7 = Node.Id(8);
+    final node9 = Node.Id(9);
+    final node10 = Node.Id(10);
+    final node11 = Node.Id(11);
+    final node12 = Node.Id(12);
+
+    graph.addEdge(node1, node2);
+    graph.addEdge(node1, node3, paint: Paint()..color = Colors.red);
+    graph.addEdge(node1, node4, paint: Paint()..color = Colors.blue);
+    graph.addEdge(node2, node5);
+    graph.addEdge(node2, node6);
+    graph.addEdge(node6, node7, paint: Paint()..color = Colors.red);
+    graph.addEdge(node6, node8, paint: Paint()..color = Colors.red);
+    graph.addEdge(node4, node9);
+    graph.addEdge(node4, node10, paint: Paint()..color = Colors.black);
+    graph.addEdge(node4, node11, paint: Paint()..color = Colors.red);
+    graph.addEdge(node11, node12);
+
+    builder
+      ..siblingSeparation = (100)
+      ..levelSeparation = (150)
+      ..subtreeSeparation = (150)
+      ..orientation = (BuchheimWalkerConfiguration.ORIENTATION_TOP_BOTTOM);
+  }
 }
