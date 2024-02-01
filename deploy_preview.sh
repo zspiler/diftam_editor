fc5395933ee1b6b259d2232fde989cfeb3145a22
Zan Spiler <zanspiler1@gmail.com>
2024-02-01 21:15:45 +0100

    move deploy script, change background color

--- deploy_preview.sh
+++ deploy_preview.sh
@@ -1,5 +1,4 @@
 BUILD_REPO="/Users/zanspiler/diploma/poc-web-preview";
-cd ..;
 flutter build web;
 sed -i '' '/^[[:space:]]*<base href="\/">[[:space:]]*$/d' build/web/index.html;
 cp -r build/web/* $BUILD_REPO;
