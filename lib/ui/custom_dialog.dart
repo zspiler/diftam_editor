2760e511e375f8ceb47856b9931868490f10a65d
Zan Spiler <zanspiler1@gmail.com>
2024-02-03 16:14:44 +0100

    Make enter shortcut submit in input dialog

--- lib/ui/custom_dialog.dart
+++ lib/ui/custom_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomDialog {
   static void showConfirmationDialog(BuildContext context,
@@ -52,6 +53,7 @@ class CustomDialog {
     }
 
     String? displayedError;
+    late FocusNode focusNode = FocusNode();
 
     // TODO AlertDialog useless? (we're not using actions)
     AlertDialog alert = AlertDialog(
@@ -63,6 +65,24 @@ class CustomDialog {
         child: Center(
           child: StatefulBuilder(
             builder: (BuildContext context, StateSetter setState) {
+              void onContinuePress() {
+                if (!acceptEmptyInput && textEditingController.text.isEmpty) {
+                  setState(() {
+                    displayedError = 'Field must not be empty';
+                  });
+                  return;
+                }
+                if (isInputValid != null && !isInputValid(textEditingController.text)) {
+                  setState(() {
+                    displayedError = errorMessage ?? 'Invalid input';
+                  });
+                  return;
+                }
+
+                onConfirm(textEditingController.text);
+                Navigator.of(context).pop();
+              }
+
               Widget cancelButton = TextButton(
                 child: Text(cancelButtonText ?? "Cancel"),
                 onPressed: () {
@@ -72,42 +92,38 @@ class CustomDialog {
 
               Widget continueButton = TextButton(
                 child: Text(confirmButtonText ?? "OK"),
-                onPressed: () {
-                  if (!acceptEmptyInput && textEditingController.text.isEmpty) {
-                    setState(() {
-                      displayedError = 'Field must not be empty';
-                    });
-                    return;
-                  }
-                  if (isInputValid != null && !isInputValid(textEditingController.text)) {
-                    setState(() {
-                      displayedError = errorMessage ?? 'Invalid input';
-                    });
-                    return;
-                  }
-                  // If validation passes or not provided
-                  onConfirm(textEditingController.text);
-                  Navigator.of(context).pop();
-                },
+                onPressed: onContinuePress,
               );
 
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextField(
-                    controller: textEditingController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: hint,
-                      errorText: displayedError, // Display the error message if not null
+              return KeyboardListener(
+                focusNode: focusNode,
+                autofocus: true,
+                onKeyEvent: (event) {
+                  if (event is KeyDownEvent && RawKeyboard.instance.keysPressed.contains(LogicalKeyboardKey.enter)) {
+                    onContinuePress();
+                  }
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: textEditingController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: hint,
+                        errorText: displayedError,
+                      ),
+                      onSubmitted: (String value) {
+                        onContinuePress(); // handle 'Enter' key press when input is focused
+                      },
                     ),
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [cancelButton, continueButton], // Place 'continueButton' inside the Row
-                  ),
-                ],
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [cancelButton, continueButton],
+                    ),
+                  ],
+                ),
               );
             },
           ),
