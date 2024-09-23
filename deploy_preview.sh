4c64198fb0e77e699833b7ccbc8edc8644ae6e42
Zan Spiler <zanspiler1@gmail.com>
2024-09-23 19:57:35 +0200

    Update deploy script to use env variable

--- deploy_preview.sh
+++ deploy_preview.sh
@@ -1,5 +1,11 @@
-BUILD_REPO="/Users/zanspiler/diploma/D2SC-editor-web-preview";
+# Script which builds the web app, copies it to a different repo and pushes changes.
+# Used to quickly re-deploy to Github Pages.
+
+if [ -z "$DIFTAM_EDITOR_BUILD_REPO" ]; then
+  echo "Environment variable DIFTAM_EDITOR_BUILD_REPO is not set";
+  exit 1;
+fi
 flutter build web;
 sed -i '' '/^[[:space:]]*<base href="\/">[[:space:]]*$/d' build/web/index.html;
-cp -r build/web/* $BUILD_REPO;
-cd $BUILD_REPO && git add . && git commit -m "update" && git push origin master;
+cp -r build/web/* $DIFTAM_EDITOR_BUILD_REPO;
+cd $DIFTAM_EDITOR_BUILD_REPO && git add . && git commit -m "update" && git push origin master;
